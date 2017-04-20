6f5a8ac6cd5b7ffe494c631b8e8682a7730da441
Daniel Soto <danielsoto317@gmail.com>
2017-04-20 01:01:41 -0500

    Commit Tracker Link in GoPro

--- app/src/main/java/com/everfox/aozoraforums/activities/GoProActivity.java
+++ app/src/main/java/com/everfox/aozoraforums/activities/GoProActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -65,6 +66,8 @@ public class GoProActivity extends AppCompatActivity {
 
         ProductPurchasePagerAdapter mSectionsPagerAdapter = new ProductPurchasePagerAdapter(getSupportFragmentManager());
         vpPro.setAdapter(mSectionsPagerAdapter);
+        ((TextView)findViewById(R.id.tvProDesc)).setMovementMethod(LinkMovementMethod.getInstance());
+
 
         goProView.setOnClickListener(new View.OnClickListener() {
             @Override
